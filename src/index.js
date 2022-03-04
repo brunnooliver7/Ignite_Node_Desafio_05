09564ed669a191d816dd6db866a2dff65e0874f7
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-03-04 00:01:04 -0300

    iniciando swagger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger.json');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const users = [];
 
