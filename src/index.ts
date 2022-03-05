38f0dac89aceace6057ab1851d5e2e3511959a16
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-03-04 23:01:09 -0300

    iniciando swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-
 import { usersRoutes } from "./routes/users.routes";
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
+import swaggerFile from "../swagger.json";
 
 app.use(express.json());
-
 app.use("/users", usersRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export { app };
